15990f89caab1b9420853ace1c832531aac4b0cb
Shady Guindi <shadyguindi13@gmail.com>
2022-11-24 22:03:38 -0500

    prototype FFT

--- Blink/FFT3AXISPROTO/FFT3AXISPROTO.ino
+++ Blink/FFT3AXISPROTO/FFT3AXISPROTO.ino
@@ -32,7 +32,7 @@ int led = 13;
 File fileX;
 File fileY; 
 File fileZ;  
-
+File dataCollection;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -44,21 +44,11 @@ void setup() {
     Serial.begin(9600);
   
   while (!SD.begin(BUILTIN_SDCARD));
-   // Serial.println("SD CARD FAILED, OR NOT PRESENT!");
-   // while (1); // don't do anything more:
   Serial.println("SD Card initialized");
 
-  // delete files if previously initialized 
-  SD.remove("arduino.txt"); 
-  SD.remove("arduino2.txt"); 
-  SD.remove("frqX.txt");
-  SD.remove("frqY.txt"); 
-  SD.remove("frqZ.txt");
-  
-  // initialize x,y,z files 
-  fileX = SD.open("frqX.txt", FILE_WRITE | FILE_READ);
-  fileY = SD.open("frqY.txt", FILE_WRITE | FILE_READ);
-  fileZ = SD.open("frqZ.txt", FILE_WRITE | FILE_READ);
+  // delete file if previously initialized
+  SD.remove("dataCollection.csv");
+  dataCollection = SD.open("dataCollection.csv", FILE_WRITE | FILE_READ);
 }
 
 void collectAnalog (int count){
@@ -73,17 +63,13 @@ void collectAnalog (int count){
   vRealX[count] = analogRead(A2);
   String zTimeStamp = String(millis());
 
-  // collecting x-axis data 
-  fileX.println(xTimeStamp+": " + vRealX[count]);
-  // collecting y-axis data
-  fileY.println(yTimeStamp+": " + vRealY[count]);
-  // collecting z-axis data
-  fileZ.println(zTimeStamp+": " + vRealZ[count]);
+  // collecting data in csv file 
+  
+  dataCollection.println(xTimeStamp + "," + yTimeStamp  + "," + zTimeStamp + "," + vRealX[count] + "," + vRealY[count] + "," + vRealZ[count]);
   vImagX[count] = 0;
   vImagY[count] = 0; 
   vImagZ[count] = 0;  
-  delay(5);
-  
+
 }
 
 double calcFFT(double vReal[],double vImag[], uint16_t samples){
@@ -98,16 +84,22 @@ double calcFFT(double vReal[],double vImag[], uint16_t samples){
 
 int counter = 0; 
 int t = millis();
-
+int periodLength = 397; 
 // the loop runs when the data is 
 void loop() {
 
   // collecting analog data until frequency is achieved 
+  int beforeExec = micros();
   collectAnalog(counter);
+  int leftOverDelay = periodLength - (micros() - beforeExec);
+  if (leftOverDelay>0){
+     delayMicroseconds(leftOverDelay);
+   } 
+ 
   counter++;
 
     if (counter == samples){
-    int t1 = micros();
+    //int t1 = micros();
     
      
      double frqX = calcFFT(vRealX, vImagX, samples);
@@ -115,7 +107,8 @@ void loop() {
      double frqZ = calcFFT(vRealZ, vImagZ, samples);
 
 
-    Serial.println(micros()-t1);
+    Serial.println(millis()-t);
+    t=millis();
      //TODO : send frequencies through CAN bus (frqX, frqY, frqZ)
      
      counter=0;
@@ -124,10 +117,3 @@ void loop() {
     }
  
   }
-//  
-//  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(100);               // wait for a second
-//  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  //delay(1);               // wait for a second
-  
-//}
